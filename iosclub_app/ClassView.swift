ceed10e36c33627f19500ff3e5ce33a179ab0bea
Jimmyluluu <luu21@kimo.com>
2020-09-07 16:54:21 +0800

    課程搞定一半

--- iosclub_app/ClassView.swift
+++ iosclub_app/ClassView.swift
@@ -10,7 +10,48 @@ import SwiftUI
 
 struct ClassView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            
+            Color(hex:"0A0A0A")
+            VStack(alignment:.leading){
+                Text("課程學習").foregroundColor(.white).font(.system(.largeTitle,design:.rounded)).tracking(6).offset(x :0,y: -280)
+                Text("/Swift學習").foregroundColor(.white).font(.system(.headline,design:.rounded)).tracking(5).offset(x :-60,y: -220)
+                Text("/Playground學習").foregroundColor(.white).font(.system(.headline,design:.rounded)).tracking(5).offset(x :-60,y: -60)
+                Text("/額外練習").foregroundColor(.white).font(.system(.headline,design:.rounded)).tracking(5).offset(x :-60,y: 100)
+            }
+            ScrollView(.horizontal){
+            HStack{
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                }
+                
+            }.offset(x :40,y: -130)
+            ScrollView(.horizontal){
+            HStack{
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                }
+            }.offset(x :40,y: 50)
+            ScrollView(.horizontal){
+            HStack{
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                Image("範例")
+                }
+            }.offset(x :40,y: 230)
+        }.edgesIgnoringSafeArea(.all)
+        
     }
 }
 
