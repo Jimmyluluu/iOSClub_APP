2311eb4d514717b36278ba7ff6bffd7eb83ff8f2
HaXAlvin <qazqwe0922@gmail.com>
2020-09-08 00:07:36 +0800

    fix activity spacing

--- iosclub_app/TimeLineView.swift
+++ iosclub_app/TimeLineView.swift
@@ -13,19 +13,25 @@ struct ActivityItem:Hashable {
     var is_school: Bool
     var date:String
 }
-var midlineLength = UIScreen.screenHeight*0.7//整個螢幕高度*0.7 中間那條線長度(37行)以及各選項間距(60行)
+var midlineLength = UIScreen.screenHeight*0.8//整個螢幕高度*0.8 中間那條線長度(37行)以及各選項間距(60行)
 
 struct TimeLineView:View {
     @State var leftData:[ActivityItem] = [
+        ActivityItem(title: "中秋夜烤",important:true,is_school:false,date:"2"),
         ActivityItem(title: "中秋夜烤",important:true,is_school:false,date:"2"),
         ActivityItem(title: "社員大會",important:true,is_school:false,date:"15")
     ]
     @State var rightData:[ActivityItem] = [
         ActivityItem(title: "迎新茶會",important:false,is_school:true,date:"1"),
         ActivityItem(title: "期末聚",important:true,is_school:true,date:"30"),
+        ActivityItem(title: "期末聚",important:true,is_school:true,date:"30"),
+        ActivityItem(title: "期末聚",important:true,is_school:true,date:"30"),
+        ActivityItem(title: "期末聚",important:true,is_school:true,date:"30"),
+        ActivityItem(title: "期末聚",important:true,is_school:true,date:"30"),
+        ActivityItem(title: "期末聚",important:true,is_school:true,date:"30"),
         ActivityItem(title: "期末聚",important:true,is_school:true,date:"30")
     ]
-    var itemSpace=0 //各項選項長度
+    var itemSpace:Int=0 //各項選項長度
     init() {
         itemSpace = leftData.count>rightData.count ? leftData.count+2:rightData.count+2 //初始化判斷左右兩邊哪一邊為最大item *警告必需要在接完後端之後
     }
@@ -35,12 +41,15 @@ struct TimeLineView:View {
             ActivityTitle(text: "社\n團", x_offset: -30)
             HStack{
                 ActivityItemListView(data: leftData,itemSpace:self.itemSpace)
+                    .offset(y:CGFloat(-50*(10-itemSpace)/10))
                 Capsule()
                     .fill(Color(hex:"bd997b"))
                     .frame(width: 10, height: midlineLength)
+                    
                     //中線
                 ActivityItemListView(data: rightData,itemSpace:self.itemSpace)
-            }
+                    .offset(y:CGFloat(-50*(10-itemSpace)/10))
+            }.offset(y:20)
         }
         .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
         .background(Color(hex:"0A0A0A"))
@@ -55,14 +64,16 @@ struct ActivityTitle:View {
         .fontWeight(.bold)
         .font(.system(.largeTitle,design:.rounded))
         .foregroundColor(Color(.white))
-        .offset(x:x_offset,y:-UIScreen.screenHeight/2*0.7)
+        .offset(x:x_offset,y:-UIScreen.screenHeight/2*0.75)
     }
 }
 struct ActivityItemListView:View {
     var data:[ActivityItem]
-    var itemSpace=0
+    var itemSpace:Int=0
     var body: some View{
-        VStack(spacing:midlineLength/CGFloat(itemSpace/*把中線長度除與左邊或右邊最大item*/)){
+        
+        VStack(spacing:(midlineLength-CGFloat(itemSpace*65*itemSpace/10))/CGFloat(itemSpace/*把中線長度除與左邊或右邊最大item*/)){
+            ActivityItemView(title: "", important: false, is_school: false, date: "").hidden()
             ForEach(data, id: \.self) { (item) in
                 ActivityItemView(title: item.title, important: item.important, is_school: item.is_school, date: item.date)
             }
