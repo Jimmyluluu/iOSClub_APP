a58612e75976a40ed3bbba3e2c7d78e51226b958
Eria <c121680118@gmail.com>
2020-09-08 07:34:18 +0800

    進行異步處理 以及決定TimeLineView內刷新頁面位置

--- iosclub_app/function/UrlHttpPostGet.swift
+++ iosclub_app/function/UrlHttpPostGet.swift
@@ -10,17 +10,20 @@ import Foundation
 
     
 
-func get(UrlSubdirectory:String/*網址子目錄*/){
+func get(UrlSubdirectory:String/*網址子目錄*/, Completion block: @escaping (NSDictionary) ->()){
     let session = URLSession(configuration: .default)//建立會議
     let url = "http://140.134.79.128:9080"+UrlSubdirectory //網址加上子目錄
     let UrlRequest = URLRequest(url: URL(string: url)!)//新增url任務
     
-    var r:NSDictionary?
+   
     let task = session.dataTask(with: UrlRequest) {(data, response, error) in
             do {
                 // 返回的是一个json，将返回的json转成字典r
-                r = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
-                print(r)
+                if var r = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary//注意 異步需搭配try在呼叫閉包closure 要不然會因為可以丟nil而持續執行
+                {
+                    block(r) //使用閉包closure 在swift裡處理異步問題等待完成時用他 他會在完成時執行
+                }
+                
             } catch {
                 // 失敗
                 print(String(data: data!, encoding: .utf8)!)
@@ -29,13 +32,8 @@ func get(UrlSubdirectory:String/*網址子目錄*/){
             }
             
         }
-        // 跑他
     
-        task.resume()
-//    if r != nil{
-//        return r!
-//    }
-//    return
+        task.resume()//跑
 }
 
 
