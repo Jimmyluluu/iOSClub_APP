62a4731781a55e23facc6d8aa92ce92e3512ac6c
HaXAlvin <qazqwe0922@gmail.com>
2020-09-09 16:02:31 +0800

    setting view fix

--- iosclub_app/SettingView.swift
+++ iosclub_app/SettingView.swift
@@ -11,20 +11,31 @@ import SwiftUI
 struct SettingView: View {
     var body: some View {
         ZStack{
-        Color(hex:"0A0A0A").edgesIgnoringSafeArea(.all)
-        VStack(alignment:.leading){
-            Text("SETTING").foregroundColor(.white).font(.system(.largeTitle,design:.rounded)).tracking(10).offset(x :0,y: -170)
-            Text("個人資料更改").foregroundColor(.white).font(.system(.headline,design:.rounded)).tracking(4).offset(x :0,y: -140)
-            Image("border_s").offset(x :0,y: -120)
-            }
+            Color(hex:"0A0A0A")
             VStack(alignment:.leading){
-                Text("生日 ：2000/04/01").foregroundColor(.white).font(.system(.body,design:.rounded)).offset(x: 5,y :-120)
-                Image("line_s").offset(x :0,y: -100)
-                Text("性別 ：女").foregroundColor(.white).font(.system(.body,design:.rounded)).offset(x: 5,y :-90)
-                Image("line_s").offset(x :0,y: -80)
-                Text("班級 ：資訊三甲").foregroundColor(.white).font(.system(.body,design:.rounded)).offset(x: 5,y :-70)
-            }
-        }
+                Text("SETTING").foregroundColor(.white).font(.system(.largeTitle,design:.rounded)).tracking(10)
+                    .padding(.bottom)
+//                    .offset(x :0,y: -170)
+                Text("個人資料更改").foregroundColor(.white).font(.system(.headline,design:.rounded)).tracking(4)
+//                    .offset(x :0,y: -140)
+                ZStack{
+                    Image("border_s")
+    //                    .offset(x :0,y: -120)
+                    VStack(spacing:30){
+                        Text("生日 ：2000/04/01").foregroundColor(.white).font(.system(.body,design:.rounded))
+        //                    .offset(x: 5,y :-120)
+                        Image("line_s")
+        //                    .offset(x :0,y: -100)
+                        Text("性別 ：男").foregroundColor(.white).font(.system(.body,design:.rounded))
+        //                    .offset(x: 5,y :-90)
+                        Image("line_s")
+        //                    .offset(x :0,y: -80)
+                        Text("班級 ：資訊三甲").foregroundColor(.white).font(.system(.body,design:.rounded))
+        //                    .offset(x: 5,y :-70)
+                    }
+                }
+            }.offset(y:-110)
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
